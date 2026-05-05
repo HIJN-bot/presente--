7fdacfc076b6dbb015e8849ad4a55b4ec0132f5d
Snt <sant85082@gmail.com>
2026-05-04 19:05:36 -0500

    fix: cambiar el tipo del argumento para la funcion del endpoint

--- back/app/routers/estudiantes/registro.py
+++ back/app/routers/estudiantes/registro.py
@@ -1,10 +1,12 @@
 #De fastapi importamos el Router de la API
 from fastapi import APIRouter
+#Importamos el archivo del esquema de estudiantes
+from schemas import estudiante_schema as es
 
 #Instanciamos el Router para crear la API
 router: APIRouter = APIRouter()
 
 #Definimos el metodo HTTP para nuestra API
 @router.post("/estudiantes")
-async def registrar_estudiante(informacion_estudiante: dict):
+async def registrar_estudiante(informacion_estudiante: es.EstudianteCreado):
     pass
