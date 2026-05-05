14ba0962aca9f00ec1fac31b9a3727fb72ab9479
Snt <sant85082@gmail.com>
2026-05-04 19:04:54 -0500

    feat: agregar el id al modelo

--- back/app/models/estudiante_model.py
+++ back/app/models/estudiante_model.py
@@ -1,13 +1,13 @@
 #Importamos de SQLAlchemy los tipos de columnas para poder definir las columnas de nuestra tabla SQL
 #(Atributos)
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 #Importamos de SQLAlchrmy Base, para poder trabajar la clase como tabla
 from app.database import Base  
 
 #Definimos la clase como esqueleto de la tabla 
 class Estudiante(Base):
     __tablename__ = "estudiantes"
-    #id: Determinado por una funcion
+    id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, nullable=False)
     apellido = Column(String, nullable=False)
     #Hacemoes que el email sea unico, tenga un indice determinado y obligarorio
