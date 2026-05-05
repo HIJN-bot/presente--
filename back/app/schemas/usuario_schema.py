6e3677396ecf06b085c55870c7fa401cd3d9c5ff
Snt <sant85082@gmail.com>
2026-05-05 14:45:09 -0500

    refactor: implementar el schema base para los usuarios

---
+++ back/app/schemas/usuario_schema.py
@@ -0,0 +1,11 @@
+#Importamos de Pydantic Base Model para validar los datos que recibimos del Front-End
+from pydantic import BaseModel
+#Importamos de Pydantic EmailStr para validar si el correo ingresado es valido
+from pydantic import EmailStr
+
+#Clase para guardar los datos del estudiante
+class UsuarioCreado(BaseModel):
+    nombre: str
+    apellido: str
+    email: EmailStr
+    contrasena: str
