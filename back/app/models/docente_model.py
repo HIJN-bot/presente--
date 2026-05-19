73feb3b5a121d2b7ff42909fed08c86827e7a35d
Salas de Sistemas ETITC <anssonitc@gmail.com>
2026-05-19 13:32:37 -0500

    feat: implementar el modelo de docente

---
+++ back/app/models/docente_model.py
@@ -0,0 +1,11 @@
+#Importamos SQLAlchemy para poder realizar el registro SQL
+from sqlalchemy import Column, String, Integer
+#Importamos la clase Base de SQLAlchemy que permite realizar los registros
+from app.database import Base
+#Importamos la clase Base del usuario
+from app.models import usuario_model as um
+
+#Definimos la clase del modelo de docente como esqueleto de la tabla
+class Docente(Base, um.Usuario):
+    __tablename__ = "docentes"
+    id = Column(Integer, primary_key=True, index=True)
