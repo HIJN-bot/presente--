f0a34d75a0ea1d354f122c6ff56263bb4ed89fd3
Salas de Sistemas ETITC <anssonitc@gmail.com>
2026-05-20 07:34:42 -0500

    feat: implementar el schema de docente

---
+++ back/app/schemas/docente_schema.py
@@ -0,0 +1,7 @@
+#Importamos pydantic para validar los datos del front
+from pydantic import BaseModel
+#Importamos el esquema base de validacion con Pydantic del usuario
+from app.schemas import usuario_schema as us
+
+class DocenteCreado(BaseModel, us):
+    pass
