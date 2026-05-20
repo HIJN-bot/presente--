d6e2825a30505164d757a0d80364b48021ee963b
Salas de Sistemas ETITC <anssonitc@gmail.com>
2026-05-20 07:34:23 -0500

    refactor: eliminar import innecesarios

--- back/app/schemas/estudiante_schema.py
+++ back/app/schemas/estudiante_schema.py
@@ -1,11 +1,7 @@
-#Importamos de Pydantic Base Model para validar los datos que recibimos del Front-End
-from pydantic import BaseModel
-#Importamos de Pydantic EmailStr para validar si el correo ingresado es valido
-from pydantic import EmailStr
-#Importamos el modelo Pydantic base Usuario
+#Importamos el modelo base Usuario ya validado con Pydantic
 import usuario_schema as us
 
 #Clase para guardar los datos del estudiante
-class EstudianteCreado(BaseModel, us.UsuarioCreado):
+class EstudianteCreado(us.UsuarioCreado):
     pass
 
