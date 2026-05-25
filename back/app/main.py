7b187df098f1f72226f9930579513bba55d5bb06
Snt <sant85082@gmail.com>
2026-05-24 22:55:31 -0500

    feat: implementar la logica del login de docentes

--- back/app/main.py
+++ back/app/main.py
@@ -5,10 +5,13 @@ from fastapi import FastAPI
 from app.routers.estudiantes import registro as registro_estudiantes
 
 # Importamos el router de login de estudiantes
-from app.routers.estudiantes import login
+from app.routers.estudiantes import login as login_estudiantes
 
 # Importamos el router de registro de docentes
 from app.routers.docentes import registro as registro_docentes
+
+#Importamos el router de login de docentes
+from app.routers.docentes import login as login_docentes
 
 # instanciamos la aplicacion de FastAPI
 app: FastAPI = FastAPI(
@@ -17,9 +20,10 @@ app: FastAPI = FastAPI(
 
 # Montamos los routers de estudiantes en la aplicacion
 app.include_router(registro_estudiantes.router, prefix="/api", tags=["estudiantes"])
-app.include_router(login.router, prefix="/api", tags=["estudiantes"])
+app.include_router(login_estudiantes.router, prefix="/api", tags=["estudiantes"])
 # Montamos los routers de los docentes en la aplicacion
 app.include_router(registro_docentes.router, prefix="/api", tags=["docentes"])
+app.include_router(login_docentes.router, prefix="/api", tags=["docentes"])
 
 
 # Funcion principal de la aplicacion
---
+++ back/app/routers/docentes/login.py
@@ -0,0 +1,64 @@
+# Importamos de FastAPI router, la exepcion HTTP y Depends
+from fastapi import APIRouter, HTTPException, Depends
+
+# Importamos el schema estudiante y modelo
+from app.schemas import docente_schema as ds
+from app.models import docente_model as dm
+
+# Imprtamos la funcion de verificar el hash, obtener la Base de datos y crear el schema para la respuesta
+from app.services.auth_service import verificar_hash
+from app.services.respuesta_usuario import crear_respuesta
+from app.database import get_db
+
+# Importamos de SQLAlchemy la funcion select
+from sqlalchemy import select
+
+# Imprtamos de SQLAlchemy las sesiones
+from sqlalchemy.orm import Session
+
+# Instanciamos el router para crear el endpoint y la API
+router: APIRouter = APIRouter()
+
+
+# Definimos el router y su metodo HTTP
+@router.post("/docentes/login", status_code=200)
+async def logear_docente(
+    informacion_docente: ds.DocenteCreado, db: Session = Depends(get_db)
+):
+    """
+    Definimos la funcion del endpoint encargada del login del docente:
+    - Recibimos la informacion del docente,
+    - verificamos en la base de datos que exista un usuario con ese correo electronico
+    - Revisamos que la constrasena coincida
+    - Retornamos el estado de la operacion y los datos importantes del docente para el cliente
+    """
+    try:
+        # Abrimos la sesion con la base de datos
+        with db as session:
+            # Definimos la query con los parametros de la consulta que queremos realizar
+            query = select(dm.Docente).where(
+                dm.Docente.email == informacion_docente.email
+            )
+            # Ejecutamos la consulta
+            docente_db: dm.Docente = session.execute(query).scalar_one_or_none()
+            # Verificamos que el usuario con ese email registrado exista en la Base de Datos
+            if docente_db is None:
+                raise HTTPException(
+                    status_code=404,
+                    detail="No se encontro un usuario para esa direccion de correo electronico",
+                )
+            # Verificamos que la contraseña sea correcta
+            if not verificar_hash(
+                informacion_docente.contrasena, docente_db.hash_contrasena
+            ):
+                raise HTTPException(status_code=400, detail="Contraseña incorrecta")
+            # Creamos el schema de respuesta
+            docente_logeado = crear_respuesta(
+                nombre=informacion_docente.nombre,
+                apellido=informacion_docente.apellido,
+                email=informacion_docente.email,
+            )
+            # Retornamos al cliente los datos necesarios del docente
+            return docente_logeado
+    except Exception:
+        raise HTTPException(status_code=400, detail="Fallo en el inicio de sesion")
