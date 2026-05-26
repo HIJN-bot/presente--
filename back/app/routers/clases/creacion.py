76880bd0d8fdf1603ff092ab88aa8eb0c358ae51
Snt <sant85082@gmail.com>
2026-05-26 17:06:19 -0500

    refactor: implementar correciones en el flujo de la funcion del endpoint, cambio en el tipo de dato de las clases en docente

--- back/app/routers/clases/creacion.py
+++ back/app/routers/clases/creacion.py
@@ -4,6 +4,9 @@ from fastapi import APIRouter, HTTPException, Depends
 # Imoprtamos de SQLAlchemy las sesiones
 from sqlalchemy.orm import Session
 
+# Importamos de SQLAlchemy select para las consultas
+from sqlalchemy import select
+
 # Importamos el ManagerClase
 from app.services.clases.manager_clases import ManagerClases
 
@@ -12,6 +15,9 @@ from app.schemas.usuarios.estudiante_schema import EstudianteCreado
 
 # Imprtamos el schema de las clases
 from app.schemas.clases.clase_schema import ClaseCreada
+
+# Importamos el schema del docente
+from app.schemas.usuarios.docente_schema import DocenteCreado
 
 # Importamos el modelo de docente
 from app.models.usuarios.docente_model import Docente
@@ -28,12 +34,14 @@ router: APIRouter = APIRouter()
 
 # Definimos el endpoint, el metodo HTTP y el status code de retorno
 @router.post("/clases/creacion", status_code=201)
-async def crear_clase(informacion_clase: ClaseCreada, db: Session = Depends(get_db)):
+async def crear_clase(informacion_clase: ClaseCreada, informacion_docente: DocenteCreado, db: Session = Depends(get_db)):
     """
     Esta funcion del endpoint se encarga de crear una clase y registrarla en la base de datos:
     - Instanciamos el ManagerClase
     - Creamos el schema de la clase
     - Creamos el modelo de la clase
+    - Consultamos con la informacion del docente
+    - Añadimos la clase en las clases que tiene el docente de la base de datos
     - Añadimos a la base de datos el modelo de la clase
     - Comprometemos a la base de datos
     - Retornamos la instancia de clase que acabamos de crear
@@ -55,13 +63,26 @@ async def crear_clase(informacion_clase: ClaseCreada, db: Session = Depends(get_
             docente_id=getattr(datos_clase, 'docente_id', None),  # Usamos getattr por si el campo no existe
             qr=datos_clase.qr.qr_image if hasattr(datos_clase.qr, 'qr_image') else datos_clase.qr
         )
+        #Creamos la consulta para obtener el docente de la base de datos
+        query = select(Docente).where(Docente.email == informacion_docente.email)
+        #Iniciamos la consulta
+        docente_db: Docente = db.execute(query).scalar_one_or_none()
+        #Revisamos si se encontro el docente
+        if docente_db is None:
+            raise HTTPException(status_code=404, detail="Docente no encontrado")
+        #Añadimos al docente de la base de datos la clase que acabamos de crear
+        docente_db.clases.append(clase)
         # Añadimos la clase a los cambios para la base de datos
         db.add(clase)
+        #Añadimos el docente a los cambios para la base de datos
+        db.add(docente_db)
         # Comprometemos los cambios en la base de datos
         db.commit()
         # Refrescamos despues del commit
         db.refresh(clase)
-        # Retornamos la instancia de la clase creada (puedes retornar el modelo o un schema de respuesta)
+        #Refrescamos despues del commit 
+        db.refresh(docente_db)
+        # Retornamos la instancia de la clase creada (puedes  retornar el modelo o un schema de respuesta)
         return clase
     except Exception:
         raise HTTPException(status_code=400, detail="Error al intentar crear la clase")
--- back/app/schemas/usuarios/docente_schema.py
+++ back/app/schemas/usuarios/docente_schema.py
@@ -7,6 +7,8 @@ from typing import List
 # Importamos el esquema base de validacion con Pydantic del usuario
 from app.schemas.usuarios import usuario_schema as us
 
+# Importamos el esquema de la clase para la lista de las clases del docente
+from app.schemas.clases.clase_schema import ClaseCreada
 
 class DocenteCreado(us.UsuarioCreado):
-    clases: List[str]
+    clases: List[ClaseCreada]
