3e4cc1d665a08a44218c55a59b5a74824fdc367f
Snt <sant85082@gmail.com>
2026-05-25 22:17:16 -0500

    feat: implementar el atributo clases en el schema y modelo de docente
    se implementa el atributo con la lista de las clases que tiene el docente

--- back/app/models/usuarios/docente_model.py
+++ back/app/models/usuarios/docente_model.py
@@ -1,6 +1,9 @@
 # Importamos SQLAlchemy para poder realizar el registro SQL
 from sqlalchemy import Column, String, Integer
 
+# Importamos de SQLAlchemy relationship para usar colecciones
+from sqlalchemy.orm import relationship
+
 # Importamos la clase Base de SQLAlchemy que permite realizar los registros
 from app.database import Base
 
@@ -12,3 +15,4 @@ from app.models.usuarios import usuario_model as um
 class Docente(um.Usuario):
     __tablename__ = "docentes"
     id = Column(Integer, primary_key=True, index=True)
+    clases = relationship("Clase", back_populates="docentes")
--- back/app/schemas/usuarios/docente_schema.py
+++ back/app/schemas/usuarios/docente_schema.py
@@ -1,9 +1,12 @@
 # Importamos pydantic para validar los datos del front
 from pydantic import BaseModel
 
+# Importamos de typing la clase List
+from typing import List
+
 # Importamos el esquema base de validacion con Pydantic del usuario
 from app.schemas.usuarios import usuario_schema as us
 
 
 class DocenteCreado(us.UsuarioCreado):
-    pass
+    clases: List[str]
